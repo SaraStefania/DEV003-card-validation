18e95b0b84ff2e2f6e45b8384e5ba3bebf0a1065
SARA <sara.sierraramos@gmail.com>
2022-12-13 18:06:17 -0500

    mejorando la interfaz y restringiendo el campo de tarjeta

--- src/index.js
+++ src/index.js
@@ -1,3 +1,87 @@
 import validator from './validator.js';
 
-console.log(validator);
+
+//capturar los elementos del HTML por medio del id
+const numerot = document.getElementById("numerot")
+const siguiente1 = document.getElementById("siguiente1")
+const siguiente2 = document.getElementById("siguiente2")
+const siguiente3 = document.getElementById("siguiente3")
+const mensajeValido = document.getElementById("mensajeValido")
+const nombre = document.getElementById("nombre")
+
+numerot.addEventListener("keydown", function(event){
+  const numbers = ["0","1","2","3","4","5","6","7","8","9", "Backspace"] //guarde en una constante los numeros - array de teclas eprmitidas
+  const isNumber = numbers.includes(event.key) //se valida si se presiono una tecla permitida(numeros)
+  if(!isNumber){
+    event.preventDefault() //se bloquea el evento y no escribe nada
+    return; 
+  }
+  
+})
+
+//hacer una funcion de ocultar y mostrar
+function ocultar(id) {
+  const pre = document.getElementById(id)
+  pre.classList.add("oculto")
+}
+  
+function mostrar(id) {
+  const pre = document.getElementById(id)
+  pre.classList.remove("oculto")
+  
+}
+
+//crear otra funcion para capturar el valor  del input y con la condicional hacer nula 
+//la validacion si la casilla input esta vacia o por el contrario me traslade a la pagina 2
+function tarjeta() {
+  const datoTarjeta = numerot.value
+  if(datoTarjeta === ""){
+    alert("Debe ingresar el numero de tarjeta")
+  }
+  else {
+ 
+    const tarjetaOculta = validator.maskify(datoTarjeta) // guardar el enmascarado
+    
+    
+    if(validator.isValid(datoTarjeta)){ //si al tarjeta es valida oculta la pagina 1 y muestro la pagina 2 con el mensaje
+      mostrar("pagina2")
+      ocultar("pagina1")
+      mensajeValido.innerHTML = nombre.value + " Su tarjeta es valida  " + tarjetaOculta // innerHTML: mostrar un mensaje en el html. Y .value tomar el valor de la casilla
+     
+
+    }
+    else { //sino muestro el alert que la tarjeta es invalida
+      alert(tarjetaOculta + nombre.value + " su tarjeta es invalida")
+
+    }
+  }
+}
+
+
+
+//crear un escuchador que este atento a la interracion del usuario con el boton
+siguiente1.addEventListener("click", tarjeta)
+
+
+//crear otras dos funciones para que se oculte y se muestre las paginas seleccionadas
+
+function paginaa3() {
+  mostrar("pagina3")
+  ocultar("pagina2")
+}
+siguiente2.addEventListener("click", paginaa3)
+
+
+function pagina31() {
+  mostrar("pagina1")
+  ocultar("pagina3")
+}
+siguiente3.addEventListener("click", pagina31)
+
+console.log(validator)
+
+
+
+
+
+
--- src/validator.js
+++ src/validator.js
@@ -1,5 +1,55 @@
-const validator = {
-  // ...
+const validator =  {
+  isValid(numerot) {
+    console.log(numerot)
+    //invertir el numero de la tarjeta 
+    const invertido = Array.from(numerot).reverse()
+    console.log(invertido)
+    //almacena todo el numero invertido y validados 
+    //recorrer el array 
+    invertido.forEach(function(numero, index){  //index es la posicion de cada numero en el array 
+      //numero es el numero de la posicion 
+
+      if(index % 2 !== 0){
+        let numero2 = numero * 2 //multiplica las posiciones pares por dos
+        if(numero2 >= 10){ 
+          const digitos = Array.from(numero2.toString())//convierte el numero en un texto para luego convertilo en array y digitos tiene 
+          numero2 = parseInt(digitos[0]) + parseInt(digitos[1]) //numero2 se convierte en la suma de sus digitos 
+        }
+        invertido[index] = numero2 //reemplaza el numero original por su multiplicacion
+      }
+    })
+    
+    console.log(invertido)
+    let sumaN = 0 //guarda la suma de los digitos de la tarjeta
+
+    invertido.forEach(function(numero){ //recorro nuevamente los digitos de la tarjeta con el foreach
+      sumaN += parseInt(numero) //se acumula la suma de cada digito
+    
+    })
+    return sumaN % 10 === 0 //si el resultado de la sumatoria es multiplo de 10 es verdadero
+    
+
+  },
+  
+  maskify(numerot) {
+    const numeroEnmascarado = Array.from(numerot) //tomar numero de la tarjeta y lo converti en array
+    const limite = numeroEnmascarado.length - 4 //calcular la posicion de los ultimos cuatro numeros. longitud del array siempre tome los ultimos cuatro 
+    return numeroEnmascarado.map(function(numero, index){ //index es la posicion de cada numero en el array 
+      //map lo convierte en un nuevo array 
+      //numero es el numero de la posicion 
+
+      return index < limite ? "#" : numero; //operador ternario, cuando el index sea menor a 12 
+      
+
+
+    }).join("") //unir todos los elementos del array 
+
+  }
 };
 
 export default validator;
+
+
+
+
+
